caea753c88a4f65fa412aaaef98188aa7c4f3a07
danieltvaz <danieltostes@live.com>
2021-07-10 23:22:57 -0300

    foo

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import ContentWrapper from "./wrappers/ContentWrapper";
 import Title from "../components/SectionTitle";
 import SectionParagraph from "../components/SectionParagraph";
