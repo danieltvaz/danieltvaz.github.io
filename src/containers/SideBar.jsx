f58fb14518cc9fb277e55b1457c8712e442c83d8
danieltvaz <danieltostes@live.com>
2021-07-21 23:51:18 -0300

    fix: minor

--- src/containers/SideBar.jsx
+++ src/containers/SideBar.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 import styled from "styled-components";
 import NavBar from "../components/NavBar";
@@ -13,22 +15,42 @@ const HeaderWrapper = styled.div`
   min-height: 100vh;
   padding: 30px 0 0 40px;
   transition: all 0.5s linear;
-
   background: ${({ theme }) => theme.foreground};
   margin-left: ${({ isOpen }) => (isOpen ? "0" : "-270px")};
   color: ${({ theme }) => theme.text};
   box-shadow: 2px 2px 5px ${({ theme }) => theme.foreground};
 
   @media screen and (min-width: 320px) and (max-width: 1023px) {
+    position: fixed;
   }
 `;
 
-const Button = styled.button`
+const StyledIcon = styled(FontAwesomeIcon)`
+  transform: rotate(135deg);
+  color: ${({ theme }) => theme.whiteText};
+  top: 5px;
+  left: 40px;
+  position: absolute;
+`;
+
+const ContentWrapper = styled.div`
+  position: fixed;
+`;
+
+const Button = styled.span`
   width: 60px;
   height: 60px;
+  display: inline;
   position: absolute;
-  left: 240px;
-  top: 60px;
+  left: 190px;
+  top: -10px;
+  transform: rotate(45deg);
+  background: ${({ theme }) => theme.foreground};
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  box-shadow: 2px 2px 5px ${({ theme }) => theme.foreground};
+  user-select: none;
 `;
 
 export default function SideBar() {
@@ -36,11 +58,14 @@ export default function SideBar() {
 
   return (
     <HeaderWrapper isOpen={isOpen}>
-      <Button onClick={() => setIsOpen((state) => !state)}>oi</Button>
+      <ContentWrapper>
+        <Button onClick={() => setIsOpen((state) => !state)}>
+          <StyledIcon icon={faBars} />
+        </Button>
 
-      <MyLogo />
-      {/* <SocialLinks /> */}
-      <NavBar />
+        <MyLogo />
+        <NavBar />
+      </ContentWrapper>
     </HeaderWrapper>
   );
 }
