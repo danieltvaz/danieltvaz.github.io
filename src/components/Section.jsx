df339930e165fe1d3c41420b6905bb03f52858de
danieltvaz <danieltostes@live.com>
2021-07-22 18:47:50 -0300

    fix: padding for sections on mobile

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,6 +5,7 @@ const Wrapper = styled.div`
   padding: 40px 80px 40px 110px;
   @media screen and (min-width: 320px) and (max-width: 1023px) {
     padding: 20px 30px 0 34px;
+    width: 100vw;
     min-height: 95vh;
   }
 `;
