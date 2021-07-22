08e1c6e3440b92abf216bc7f116c9016087ac37c
danieltvaz <danieltostes@live.com>
2021-07-21 23:50:47 -0300

    fix: minor

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -3,14 +3,11 @@ import styled from "styled-components";
 const Wrapper = styled.div``;
 
 const Title = styled.h3`
-  font-size: 2vmax;
+  font-size: 28px;
   color: ${({ theme }) => theme.text};
   max-width: 70px;
   margin-bottom: 20px;
   border-bottom: 2px solid ${({ theme }) => theme.highlight};
-  @media screen and (min-width: 320px) and (max-width: 1023px) {
-    margin: 0 auto 5vmax auto;
-  }
 `;
 
 export default function SectionTitle({ children }) {
