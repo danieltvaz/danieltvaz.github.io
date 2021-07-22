102e762509ab387cc4b5c01d1d743ded8922df9e
danieltvaz <danieltostes@live.com>
2021-07-21 23:50:41 -0300

    fix: adjust size for mobile

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,13 +1,11 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  /* min-height: 100vh; */
-  /* margin-left: 17vmax; */
   background: ${({ theme }) => theme.background};
-  padding: 40px 80px 0px;
+  padding: 40px 80px 40px 110px;
   @media screen and (min-width: 320px) and (max-width: 1023px) {
-    /* margin-left: 0;
-    padding: 3vmax 1vmax 4vmax 1vmax; */
+    padding: 20px 30px 0 34px;
+    min-height: 95vh;
   }
 `;
 
