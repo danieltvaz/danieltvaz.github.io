bbc04bf9814cb7ec31ba09cb95fcee12f6b4266b
danieltvaz <danieltostes@live.com>
2021-07-23 01:02:31 -0300

    fix: semantic tags applyed & height fix

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const Wrapper = styled.section`
   background: ${({ theme }) => theme.background};
   padding: 40px 80px 40px 110px;
+  min-height: 100vh;
+
   @media screen and (min-width: 320px) and (max-width: 1023px) {
     padding: 20px 30px 0 34px;
     width: 100vw;
-    min-height: 95vh;
   }
 `;
 
