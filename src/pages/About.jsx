efd8f9f9315c4e6b08fffc718c50edd9186789b4
danieltvaz <danieltostes@live.com>
2021-07-24 15:48:40 -0300

    feat: about photo updated with design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,25 +1,46 @@
 import styled from "styled-components";
 import Section from "../components/Section";
 import SectionTitle from "../components/SectionTitle";
-import me from "../assets/meb&w.png";
+
+import me2 from "../assets/me2.png";
 
 const Paragraph = styled.p`
   color: ${({ theme }) => theme.text};
   margin-bottom: 10px;
-  max-width: 90%;
+  max-width: 500px;
+`;
+
+const ImgWrapper = styled.div`
+  background: ${({ theme }) => theme.highlight};
+  position: relative;
+
+  left: 80px;
+  top: -50px;
+  width: 300px;
+  height: 600px;
+  @media screen and (min-width: 320px) and (max-width: 1023px) {
+    height: 400px;
+    width: 200px;
+    top: 0;
+    left: 0;
+  }
+
+  img {
+    width: 400px;
+    /* transform: translate(-50%, -50%); */
+    position: absolute;
+    top: 56px;
+    left: 0;
+    @media screen and (min-width: 320px) and (max-width: 1023px) {
+      width: 300px;
+      top: -9px;
+    }
+  }
 `;
 
 const MeWrapper = styled.div`
   display: flex;
 
-  img {
-    max-width: 400px;
-    border-bottom-right-radius: 30%;
-    border-bottom-left-radius: 5%;
-    @media screen and (min-width: 320px) and (max-width: 1023px) {
-      max-width: 300px;
-    }
-  }
   @media screen and (min-width: 320px) and (max-width: 1023px) {
     flex-direction: column;
     align-items: center;
@@ -50,7 +71,9 @@ export default function About() {
             responsivas e com as melhores praticas do mercado.
           </Paragraph>
         </TextWrapper>
-        <img src={me} style={{ width: "500px" }} alt="daniel tostes" />
+        <ImgWrapper>
+          <img src={me2} alt="daniel tostes" />
+        </ImgWrapper>
       </MeWrapper>
     </Section>
   );
