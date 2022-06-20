d04df8423b350246c8544f5b45963e1529dc4828
danieltvaz <danieltostes@live.com>
2022-06-20 19:27:07 -0300

    update: Skills update

---
+++ src/constants/skillsData.js
@@ -0,0 +1,71 @@
+import htmlIcon from "../assets/skills/html5.png";
+import css3Icon from "../assets/skills/css3.png";
+import javascriptIcon from "../assets/skills/javascript.png";
+import styledcomponentsIcon from "../assets/skills/styledcomponents.png";
+import reactjsIcon from "../assets/skills/reactjs.png";
+import gitIcon from "../assets/skills/git.png";
+import nodejsIcon from "../assets/skills/nodejs.png";
+import scrumIcon from "../assets/skills/scrum.png";
+import linuxIcon from "../assets/skills/linux.png";
+import vueIcon from "../assets/skills/vue.png";
+import sassIcon from "../assets/skills/sass.png";
+import typescriptIcon from "../assets/skills/typescript.png";
+import reactnativeIcon from "../assets/skills/reactnative.png";
+
+export const skillsData = [
+  {
+    name: "javascript",
+    icon: javascriptIcon,
+  },
+  {
+    name: "typescript",
+    icon: typescriptIcon,
+  },
+  {
+    name: "react",
+    icon: reactjsIcon,
+  },
+  {
+    name: "react native",
+    icon: reactnativeIcon,
+  },
+  {
+    name: "html",
+    icon: htmlIcon,
+  },
+  {
+    name: "css3",
+    icon: css3Icon,
+  },
+
+  {
+    name: "VueJS",
+    icon: vueIcon,
+  },
+
+  {
+    name: "styled components",
+    icon: styledcomponentsIcon,
+  },
+  {
+    name: "Sass",
+    icon: sassIcon,
+  },
+
+  {
+    name: "git",
+    icon: gitIcon,
+  },
+  {
+    name: "nodejs",
+    icon: nodejsIcon,
+  },
+  {
+    name: "scrum",
+    icon: scrumIcon,
+  },
+  {
+    name: "linux",
+    icon: linuxIcon,
+  },
+];
